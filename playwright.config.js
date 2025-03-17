1b1d6219e1a1187860c0690bd01c0c4c9df4d7b9
Maximka47 <user2017max@gmail.com>
2025-03-18 01:22:13 +0200

    playwright config and workflow config updated

--- playwright.config.js
+++ playwright.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   reporter: [['line'],['allure-playwright']],
+  use: {
+    trace: 'on', // Optional: Enable tracing for debugging
+  },
   testDir: './tests',
 });
 
