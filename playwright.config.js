a6d043e9a7e80a3cdbb04c0b4c4eadb2c848fc3a
Maximka47 <user2017max@gmail.com>
2025-03-17 22:36:27 +0200

    playwright.yml update

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 
 
 export default defineConfig({
-  reporter: [['line'], ['allure-playwright']],
+  reporter: [['line'],['allure-playwright']],
   testDir: './tests',
 });
 
